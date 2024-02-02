056de9b79468641a0642823ba83c72630d4856bb
Ne4upara <ne4upara84@gmail.com>
2024-02-02 14:45:40 +0200

    Initial commit

---
+++ src/test/java/ua/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
