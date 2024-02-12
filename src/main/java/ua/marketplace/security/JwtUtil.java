e4e3b4b45ca011ae05904f40ae435ab897497df3
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2024-02-12 12:20:41 +0100

    fix tests

--- src/main/java/ua/marketplace/security/JwtUtil.java
+++ src/main/java/ua/marketplace/security/JwtUtil.java
@@ -23,7 +23,7 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class JwtUtil {
 
-    private static final String secretKey = "${JWT_SECRET}";
+    private static final String secretKey = "${{ secrets.JWT_SECRET }}";
     private final UserDetailsService userDetailsService;
 
     /**
