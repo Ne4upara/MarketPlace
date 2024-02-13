bce4c1a49f26d6ab0b40124c8c0927f58321852a
andriiiiiko <andrii.protas@outlook.com>
2024-02-13 14:56:44 +0100

    Change methods equals and hashcode to the lombok annotation

--- src/main/java/ua/marketplace/entities/User.java
+++ src/main/java/ua/marketplace/entities/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 /**
  * An entity class representing a user in the system.
@@ -17,6 +16,7 @@ import java.util.Objects;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class User {
 
     @Id
@@ -24,7 +24,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "phone_numeber")
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     @Column(name = "password")
@@ -41,29 +41,4 @@ public class User {
 
     @Column(name = "is_enabled")
     private Boolean isEnabled;
-
-    /**
-     * Compares this User with the specified object for equality.
-     * Two User objects are considered equal if they have the same id, phone, and password.
-     *
-     * @param object the object to be compared for equality with this User
-     * @return true if the specified object is equal to this User, otherwise false
-     */
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        User user = (User) object;
-        return Objects.equals(id, user.id) && Objects.equals(phoneNumber, user.phoneNumber);
-    }
-
-    /**
-     * Returns a hash code value for this User.
-     *
-     * @return a hash code value for this User
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, phoneNumber);
-    }
 }
