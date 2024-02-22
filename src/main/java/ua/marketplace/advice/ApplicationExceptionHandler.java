d6b4a7ac118f7e7ad6a39bae7fc85c417aeb5d09
Ne4upara <ne4upara84@gmail.com>
2024-02-22 11:16:46 +0200

    test exception handler

--- src/main/java/ua/marketplace/advice/ApplicationExceptionHandler.java
+++ src/main/java/ua/marketplace/advice/ApplicationExceptionHandler.java
@@ -2,6 +2,7 @@ package ua.marketplace.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ua.marketplace.exception.AppException;
 import java.util.HashMap;
@@ -11,9 +12,18 @@ import java.util.Map;
 public class ApplicationExceptionHandler {
 
     @ExceptionHandler(AppException.class)
-    public ResponseEntity<Map<String , String>> handlerPhoneAlreadyExist(Exception ex){
+    public ResponseEntity<Map<String , String>> appException(Exception ex){
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("errorMessage", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String , String>> handleInvalidArgument(MethodArgumentNotValidException ex){
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errorMap.put(error.getField(),error.getDefaultMessage())
+        );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap);
     }
 }
