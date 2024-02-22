5d52a9b95555bffb49787603e840cbcc9e6e92a6
Ne4upara <ne4upara84@gmail.com>
2024-02-22 10:55:29 +0200

    test exception handler

--- src/main/java/ua/marketplace/services/GoogleRegistrationService.java
+++ src/main/java/ua/marketplace/services/GoogleRegistrationService.java
@@ -1,14 +1,14 @@
-package ua.marketplace.services;
-
-import org.springframework.http.ResponseEntity;
-import ua.marketplace.dto.GoogleDto;
-import ua.marketplace.requests.GoogleRequest;
-import ua.marketplace.responses.CustomResponse;
-
-public class GoogleRegistrationService implements IGoogleRegistrationService {
-
-    @Override
-    public ResponseEntity<CustomResponse<GoogleDto>> googleOK(GoogleRequest request) {
-        return null;
-    }
-}
+//package ua.marketplace.services;
+//
+//import org.springframework.http.ResponseEntity;
+//import ua.marketplace.dto.GoogleDto;
+//import ua.marketplace.requests.GoogleRequest;
+//import ua.marketplace.responses.CustomResponse;
+//
+//public class GoogleRegistrationService implements IGoogleRegistrationService {
+//
+//    @Override
+//    public ResponseEntity<CustomResponse<GoogleDto>> googleOK(GoogleRequest request) {
+//        return null;
+//    }
+//}
--- src/main/java/ua/marketplace/services/IGoogleRegistrationService.java
+++ src/main/java/ua/marketplace/services/IGoogleRegistrationService.java
@@ -1,11 +1,11 @@
-package ua.marketplace.services;
-
-import org.springframework.http.ResponseEntity;
-import ua.marketplace.dto.GoogleDto;
-import ua.marketplace.requests.GoogleRequest;
-import ua.marketplace.responses.CustomResponse;
-
-public interface IGoogleRegistrationService {
-
-    ResponseEntity<CustomResponse<GoogleDto>> googleOK(GoogleRequest request);
-}
+//package ua.marketplace.services;
+//
+//import org.springframework.http.ResponseEntity;
+//import ua.marketplace.dto.GoogleDto;
+//import ua.marketplace.requests.GoogleRequest;
+//import ua.marketplace.responses.CustomResponse;
+//
+//public interface IGoogleRegistrationService {
+//
+//    ResponseEntity<CustomResponse<GoogleDto>> googleOK(GoogleRequest request);
+//}
