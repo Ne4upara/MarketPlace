a915f6f96a1ec489f669fddecd48150fd3e1bd5b
Ne4upara <ne4upara84@gmail.com>
2024-02-25 10:35:55 +0200

    fix Request

--- src/main/java/ua/marketplace/requests/PhoneCodeRequest.java
+++ src/main/java/ua/marketplace/requests/PhoneCodeRequest.java
@@ -1,7 +1,5 @@
 package ua.marketplace.requests;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -16,8 +14,11 @@ import lombok.*;
 @AllArgsConstructor
 public class PhoneCodeRequest {
 
-    @NotEmpty(message = "Phone should not be empty")
-    @NotNull(message = "Phone should not be null")
+    /**
+     * The user enters the code.
+     * Validation constraints:
+     * - The code only needs to contain exactly 4 digits.
+     */
     @Size(min = 4, max = 4, message = "Phone should be between 4 digits")
     @Pattern(regexp = "^\\d+$", message = "Phone should contain only digits")
     private String inputCode;
@@ -25,12 +26,9 @@ public class PhoneCodeRequest {
     /**
      * The phone number of the user.
      * Validation constraints:
-     * - The phone number should not be empty and not null.
      * - The phone number should contain exactly 13 digits.
      * - The phone number should be in the format +380xxxxxxxxx.
      */
-    @NotEmpty(message = "Phone should not be empty")
-    @NotNull(message = "Phone should not be null")
     @Size(min = 13, max = 13, message = "Phone should be between 13 digits")
     @Pattern(regexp = "^\\+380\\d+$", message = "Phone should contain only digits and should be in the format +380..")
     private String phoneNumber;
