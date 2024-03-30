17b50c31037bd2500e4e1205ab0eb249e5d3dbd3
Ne4upara <ne4upara84@gmail.com>
2024-03-30 11:44:25 +0200

    fix swagger 403 -> 401

--- src/main/java/ua/marketplace/controllers/IProductController.java
+++ src/main/java/ua/marketplace/controllers/IProductController.java
@@ -49,7 +49,7 @@ public interface IProductController {
             @ApiResponse(responseCode = "201", description = "Product created successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid input",
                     content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class))),
-            @ApiResponse(responseCode = "403", description = "Not authorized user",
+            @ApiResponse(responseCode = "401", description = "Not authorized user",
                     content = @Content())
     })
     ProductDto createProduct
@@ -65,7 +65,7 @@ public interface IProductController {
                     content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class))),
             @ApiResponse(responseCode = "404", description = "Product not found",
                     content = @Content(schema = @Schema(implementation = ErrorMessageResponse.class))),
-            @ApiResponse(responseCode = "403", description = "User not authorized",
+            @ApiResponse(responseCode = "401", description = "User not authorized",
                     content = @Content()),
             @ApiResponse(responseCode = "409", description = "This product was not created by this user",
                     content = @Content(schema = @Schema(implementation = ErrorMessageResponse.class)))
@@ -83,7 +83,7 @@ public interface IProductController {
             @ApiResponse(responseCode = "200", description = "Product rated successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid input",
                     content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class))),
-            @ApiResponse(responseCode = "403", description = "User not authorized",
+            @ApiResponse(responseCode = "401", description = "User not authorized",
                     content = @Content()),
             @ApiResponse(responseCode = "404", description = "Product not found",
                     content = @Content(schema = @Schema(implementation = ErrorMessageResponse.class)))
@@ -96,7 +96,7 @@ public interface IProductController {
     @Operation(summary = "Delete a product", description = "Endpoint to delete a product")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Product deleted successfully"),
-            @ApiResponse(responseCode = "403", description = "User not authorized",
+            @ApiResponse(responseCode = "401", description = "User not authorized",
                     content = @Content()),
             @ApiResponse(responseCode = "404", description = "Product not found",
                     content = @Content(schema = @Schema(implementation = ErrorMessageResponse.class))),
