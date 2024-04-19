aff7774014ba13d2e7e766193deebc6a9caff3dc
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2024-04-19 15:20:43 +0200

    refactored tests

--- src/test/java/ua/marketplace/config/TestCacheConfig.java
+++ src/test/java/ua/marketplace/config/TestCacheConfig.java
@@ -4,13 +4,15 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 
 @TestConfiguration
 public class TestCacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
+    @Primary
+    public CacheManager testCacheManager() {
         return new ConcurrentMapCacheManager();
     }
 }
--- src/test/java/ua/marketplace/controllers/ProductControllerTest.java
+++ src/test/java/ua/marketplace/controllers/ProductControllerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.marketplace.config.TestCacheConfig;
 import ua.marketplace.dto.Pagination;
 import ua.marketplace.entities.Category;
 import ua.marketplace.entities.Product;
@@ -27,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser(username = "test", password = "test")
+@Import(TestCacheConfig.class)
 class ProductControllerTest {
 
     @Autowired
