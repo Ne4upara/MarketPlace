fac896835311911648d8a066d58aaf210eae956c
Vladyslav Malovanyi <127311448+vldMlvn@users.noreply.github.com>
2024-04-25 12:53:14 +0200

    comment

--- src/main/java/ua/marketplace/requests/RegistrationRequest.java
+++ src/main/java/ua/marketplace/requests/RegistrationRequest.java
@@ -5,13 +5,22 @@ import jakarta.validation.constraints.Size;
 
 /**
  * Represents a registration request.
+ * This class contains two fields: `firstName` and `phoneNumber`.
+ * Both fields are annotated with validation constraints to ensure that the user-provided data meets certain criteria.
  */
 public record RegistrationRequest(
-        @Size(min = 2, max = 15, message = "Name should be between 2 and 15 characters")
-        @Pattern(regexp = "^[a-zA-Zа-яА-ЯіІїЇєЄґҐ]+$", message = "Name should contain only letters (Latin or Cyrillic)")
+
+        // `firstName` field
+        @Size(min = 2, max = 15, 
+            message = "Name should be between 2 and 15 characters")
+        @Pattern(regexp = "^[a-zA-Zа-яА-ЯіІїЇєЄґҐ]+$", 
+            message = "Name should contain only letters (Latin or Cyrillic)")
         String firstName,
 
-        @Size(min = 13, max = 13, message = "Phone should be between 13 digits")
-        @Pattern(regexp = "^\\+380\\d+$", message = "Phone should contain only digits and should be in the format +380..")
+        // `phoneNumber` field
+        @Size(min = 13, max = 13, 
+            message = "Phone should be between 13 digits")
+        @Pattern(regexp = "^\\+380\\d+$", 
+            message = "Phone should contain only digits and should be in the format +380..")
         String phoneNumber) {
 }
