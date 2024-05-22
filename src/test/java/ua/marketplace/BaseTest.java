0066a9e57ae81fe05a2e934b184ae873bc964f8e
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2024-05-22 19:36:22 +0200

    fix BaseTest

--- src/test/java/ua/marketplace/BaseTest.java
+++ src/test/java/ua/marketplace/BaseTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import ua.marketplace.config.TestCacheConfig;
-import ua.marketplace.config.TestSecurityConfig;
 import ua.marketplace.entities.User;
 import ua.marketplace.repositoryes.UserRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(locations="classpath:application-dev.properties")
+@Import(TestCacheConfig.class)
 @Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @WithMockUser(value = "testuser")
